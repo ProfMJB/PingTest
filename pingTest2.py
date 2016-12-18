f9c0d864eccb1e25e2dc8feb69f84eba5f8a1d1b
Max <max@thetroll.org.uk>
2016-12-18 16:55:39 +0000

    Write to file

--- pingTest2.py
+++ pingTest2.py
@@ -1,14 +1,17 @@
-import subprocess
+import subprocess, time
 
 hostname = "google.co.uk"
-proc = subprocess.Popen("ping " + hostname, stdout=subprocess.PIPE, shell=True)
-(out, err) = proc.communicate()
-if out[0:6] == "Ping r":
-    print("Fail")
-else:
-    print("Success")
-    parse = out.split("time=")
-    time = parse[1].split("ms")
-    print("time : "+time[0])
-print(out)
-print(out[0:6])
+while True:
+    proc = subprocess.Popen("ping " + hostname, stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    if out[0:6] == "Ping r":
+        with open("log.txt", "a") as thisfile:
+            thisfile.write(str(time.strftime("%c"))+" Fail\n")
+        print("Fail")
+    else:
+        parse = out.split("time=")
+        pingtime = parse[1].split("ms")
+        with open("log.txt", "a") as thisfile:
+            thisfile.write(str(time.strftime("%c")) + " Success. Time : "+ pingtime[0]+"ms\n")
+        print("Success")
+    time.sleep(30)
